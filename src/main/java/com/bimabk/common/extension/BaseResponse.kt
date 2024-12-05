cc60106f04371322b49e9280a6299f23a1c5dc10
bima bagaskhoro <bima.bagaskhoro@gmail.com>
2024-12-05 17:40:17 +0700

    fix testing first open apps

--- src/main/java/com/bimabk/common/extension/BaseResponse.kt
+++ src/main/java/com/bimabk/common/extension/BaseResponse.kt
@@ -3,7 +3,7 @@ package com.bimabk.common.extension
 import com.google.gson.annotations.SerializedName
 
 data class BaseResponse<T>(
-    @SerializedName(value = "status") val status: String?,
+    @SerializedName("status") val status: String?,
     @SerializedName("message") val message: String?,
     @SerializedName("data") val data: T?
 )
